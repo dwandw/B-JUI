0d16c1853c800855f43cc0bb50b60f4a69f87523
dwandw <dwandw@gmail.com>
2015-11-21 18:59:31 +0800

    项目一些工具、说明更新

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,12 @@
 var gulp = require('gulp');
-var clean = require('gulp-clean');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
-var staticHash = require('gulp-static-hash');
-var minimist = require('minimist');
-
-var knownOptions = {
-    string: 'file',
-    default: {
-        env: process.env.NODE_ENV || 'production'
-    }
-};
-
-var options = minimist(process.argv.slice(2), knownOptions);
-
-gulp.task('s', function() {
-    var file = '/bjui-' + options.file + '.js';
-    return gulp.src(paths.src + file)
-        .pipe(uglify({
-            output: {
-                max_line_len: 0
-            },
-            preserveComments: function(node, comment) {
-                console.log(comment.value);
-                console.log(comment.type);
-                if (/^\/\* =+/.test(comment.value) && /\* =+$/.test(comment.value)) {
-                    return true;
-                }
-            }
-        }))
-        .pipe(concat(file + ".bak"))
-        .pipe(gulp.dest(paths.dist));
-});
 
 var paths = {
-    src: 'BJUI/js',
-    dist: 'BJUI/js'
+    src: 'BJUI/js'
 };
 
-var file = [
+var files = [
     paths.src + '/bjui-core.js',
     paths.src + '/bjui-regional.zh-CN.js',
     paths.src + '/bjui-frag.js',
@@ -64,26 +32,20 @@ var file = [
     paths.src + '/bjui-upload.js',
     paths.src + '/bjui-theme.js',
     paths.src + '/bjui-initui.js',
-    paths.src + '/bjui-plugins.js'
+    paths.src + '/bjui-plugins.js',
+    paths.src + '/bjui-drag.js',
+    paths.src + '/bjui-address.js'
 ];
 
 gulp.task('build', function() {
-    gulp.src(file)
+    gulp.src(files)
         .pipe(uglify({
             output: {
                 max_line_len: 0
             }
         }))
         .pipe(concat('bjui-all.js'))
-        .pipe(gulp.dest(paths.dist));
-});
-
-// 清洁工作
-gulp.task('clean', function() {
-    gulp.src(paths.dist)
-        .pipe(clean({
-            force: true
-        }));
+        .pipe(gulp.dest(paths.src));
 });
 
 gulp.task('default', ['build']);
